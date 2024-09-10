cf3718d8dde6fda2516fcd926558231d0d0e0443
yfelipee <104224062+yfelipee@users.noreply.github.com>
2024-09-10 20:00:16 -0300

    Update Glam.sql

--- backend/Glam.sql
+++ backend/Glam.sql
@@ -6,7 +6,7 @@ Create table dados (
   id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     endereco VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
 );
 
 INSERT INTO dados (nome, endereco, email) VALUES ('João Silva', 'Rua das Flores, 123', 'joao.silva@example.com');
