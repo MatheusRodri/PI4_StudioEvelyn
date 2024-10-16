3028e906af4782efc6e2b6fe72ed93885b655ab9
yfelipee <felipe.macena1103@gmail.com>
2024-10-15 22:08:32 -0300

    Home

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -3,13 +3,58 @@ import './index.css';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
+
 export default function Home() {
   return (
-    <div>
+    <>
       <Header />
-      {/* Conteúdo da página Home */}
+      
+    <div className="container">
+  {/* Seção da primeira imagem com texto */}
+  <div className="section">
+    <img
+      src="images/henna.png"
+      alt="Imagem 1: Empreendedora e sua história"
+      className="image"
+    />
+    <div className="textSection">
+      <h2 className="tituloh2">Heading</h2>
+   
+      <p>
+        Excepteur efficient emerging, minim veniam anim aute carefully curated
+        Ginza conversation exquisite perfect nostrud nisi intricate Content.
+      </p>
+      <p>
+        Punctual adipiscing, essential lovely queem tempor eiusmod irure. 
+        Exclusive izakaya charming Scandinavian impeccable aute quality of 
+        life soft power pariatur Melbourne occaecat discerning.
+      </p>
+    </div>
+  </div>
+
+  {/* Seção da segunda imagem com texto */}
+  <div className="section">
+    <div className="textSection">
+      <h2 className="tituloh2">Heading</h2>
+     
+      <p>
+        Body text for your whole article or post. We’ll put in some lorem ipsum
+        to show how a filled-out page might look:
+      </p>
+      <p>
+        Qui wardrobe aliquip, et Porter destination Toto remarkable officia Helsinki
+        exccepteur Basset hound. Zürich sleepy perfect consectetur.
+      </p>
+    </div>
+    <img
+      src="images/henna.png"
+      alt="Imagem 2: Destaque de um procedimento"
+      className="image"
+    />
+  </div>
+</div>
       
       <Footer/>
-    </div>
+    </>
   );
 }
