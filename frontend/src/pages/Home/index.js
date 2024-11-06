234ef60131b8bb1387aeb6b165652d8cfee065e6
Emily Gomes <emily.45gomes@hotmail.com>
2024-11-05 21:23:31 -0300

    Modificação da tela home, adicionando mais textos e imagens.

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -13,21 +13,18 @@ export default function Home() {
   {/* Seção da primeira imagem com texto */}
   <div className="section">
     <img
-      src="images/henna.png"
+      src="images/home1.png"
       alt="Imagem 1: Empreendedora e sua história"
       className="image"
     />
     <div className="textSection">
-      <h2 className="tituloh2">Heading</h2>
+      <h2 className="tituloh2">Seja Bem Vinda(o)!!</h2>
    
             <p className='vw-plugin-top-wrapper'>
-        Excepteur efficient emerging, minim veniam anim aute carefully curated
-        Ginza conversation exquisite perfect nostrud nisi intricate Content.
+        Nosso propósito é lhe proporcionar as melhores experiências em extensão de cílios! Entregando um resultado lindo e de alta performance!
       </p>
       <p>
-        Punctual adipiscing, essential lovely queem tempor eiusmod irure. 
-        Exclusive izakaya charming Scandinavian impeccable aute quality of 
-        life soft power pariatur Melbourne occaecat discerning.
+        Temos diversos modelos de extensão de cílios para você se apaixonar! Nossos modelos vão desde um resultado mais natural a um resultado com mais volume. Você escolhe!
       </p>
     </div>
   </div>
@@ -35,25 +32,67 @@ export default function Home() {
   {/* Seção da segunda imagem com texto */}
   <div className="section">
     <div className="textSection">
-      <h2 className="tituloh2">Heading</h2>
+      <h2 className="tituloh2">Segurança é a nossa base!</h2>
      
       <p>
-        Body text for your whole article or post. We’ll put in some lorem ipsum
-        to show how a filled-out page might look:
+        No Studio Evelyn Gomes Cílios e Sobrancelhas trabalhamos sempre com segurança e cautela, respeitando os limites dos cílios de nossas clientes.
       </p>
       <p>
-        Qui wardrobe aliquip, et Porter destination Toto remarkable officia Helsinki
-        exccepteur Basset hound. Zürich sleepy perfect consectetur.
+        Todos os nossos materiais são esterelizados e higienizados, além de um ambiente seguro e confortável.
       </p>
     </div>
     <img
-      src="images/henna.png"
-      alt="Imagem 2: Destaque de um procedimento"
+      src="images/home2.png" 
+      alt="Imagem da execução do serviço de cílios: Destaque de um procedimento"
       className="image"
     />
   </div>
-</div>
+
+  <div className="section">
+  <img
+      src="images/home3.png" 
+      alt="Imagem da execução do serviço de cílios: Destaque de um procedimento"
+      className="image"
+    />
+    <div className="textSection">
+      <h2 className="tituloh2">Labios mais Harmonizados!</h2>
       
+      <p>
+        Com o nosso serviço de micropigmentação labial, você terá os labios mais harmônicos e com mais cor!
+      </p>
+      <p>
+       Temos cores mais suaves, como um rosado mais natural até um vermelho marcante. Entre na nossa página de serviços disponível em nosso site e fique a vontade!
+      </p>
+    </div>
+    
+  </div>
+  <div className="section">
+    <div className="textSection">
+      <h2 className="tituloh2">Sobrancelhas bem definidas!</h2>
+     
+      <p>
+        Na categoria sobrancelhas, temos serviços de micropigmentação, design simples e design com henna.
+      </p>
+      <p>
+        Tudo depende da sua necessidade e o que mais te agrada!
+        No canto superior direito de nosso site, está disponível a página de serviços onde você, nossa cliente, pode esolher o que quiser!
+      </p>
+    </div>
+    <img
+      src="images/home4.png" 
+      alt="Imagem da execução do serviço de cílios: Destaque de um procedimento"
+      className="image"
+    />
+  </div>
+
+</div>
+
+{/* Seção da segunda imagem com texto */}
+
+
+  {/* Seção da segunda imagem com texto */}
+
+
       <Footer/>
     </>
   );
