f98e6950976867e9dd1aea8fa2fdf678ca3a2f0a
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2024-11-05 22:40:19 -0300

    Update index.js

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './index.css'; 
+import './index.css';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
@@ -9,91 +9,86 @@ export default function Home() {
     <>
       <Header />
 
-    <div className="container">
-  {/* Seção da primeira imagem com texto */}
-  <div className="section">
-    <img
-      src="images/home1.png"
-      alt="Imagem 1: Empreendedora e sua história"
-      className="image"
-    />
-    <div className="textSection">
-      <h2 className="tituloh2">Seja Bem Vinda(o)!!</h2>
-   
+      <div className="container">
+        {/* Seção da primeira imagem com texto */}
+        <div className="section">
+          <img
+            src="images/home1.png"
+            alt="Imagem 1: Empreendedora e sua história"
+            className="image"
+          />
+          <div className="textSection">
+            <h2 className="tituloh2">Seja Bem Vinda(o)!!</h2>
+
             <p className='vw-plugin-top-wrapper'>
-        Nosso propósito é lhe proporcionar as melhores experiências em extensão de cílios! Entregando um resultado lindo e de alta performance!
-      </p>
-      <p>
-        Temos diversos modelos de extensão de cílios para você se apaixonar! Nossos modelos vão desde um resultado mais natural a um resultado com mais volume. Você escolhe!
-      </p>
-    </div>
-  </div>
+              Nosso propósito é lhe proporcionar as melhores experiências em extensão de cílios! Entregando um resultado lindo e de alta performance!
+            </p>
+            <p>
+              Temos diversos modelos de extensão de cílios para você se apaixonar! Nossos modelos vão desde um resultado mais natural a um resultado com mais volume. Você escolhe!
+            </p>
+          </div>
+        </div>
 
-  {/* Seção da segunda imagem com texto */}
-  <div className="section">
-    <div className="textSection">
-      <h2 className="tituloh2">Segurança é a nossa base!</h2>
-     
-      <p>
-        No Studio Evelyn Gomes Cílios e Sobrancelhas trabalhamos sempre com segurança e cautela, respeitando os limites dos cílios de nossas clientes.
-      </p>
-      <p>
-        Todos os nossos materiais são esterelizados e higienizados, além de um ambiente seguro e confortável.
-      </p>
-    </div>
-    <img
-      src="images/home2.png" 
-      alt="Imagem da execução do serviço de cílios: Destaque de um procedimento"
-      className="image"
-    />
-  </div>
+        {/* Seção da segunda imagem com texto */}
+        <div className="section">
+          <div className="textSection">
+            <h2 className="tituloh2">Segurança é a nossa base!</h2>
 
-  <div className="section">
-  <img
-      src="images/home3.png" 
-      alt="Imagem da execução do serviço de cílios: Destaque de um procedimento"
-      className="image"
-    />
-    <div className="textSection">
-      <h2 className="tituloh2">Labios mais Harmonizados!</h2>
-      
-      <p>
-        Com o nosso serviço de micropigmentação labial, você terá os labios mais harmônicos e com mais cor!
-      </p>
-      <p>
-       Temos cores mais suaves, como um rosado mais natural até um vermelho marcante. Entre na nossa página de serviços disponível em nosso site e fique a vontade!
-      </p>
-    </div>
-    
-  </div>
-  <div className="section">
-    <div className="textSection">
-      <h2 className="tituloh2">Sobrancelhas bem definidas!</h2>
-     
-      <p>
-        Na categoria sobrancelhas, temos serviços de micropigmentação, design simples e design com henna.
-      </p>
-      <p>
-        Tudo depende da sua necessidade e o que mais te agrada!
-        No canto superior direito de nosso site, está disponível a página de serviços onde você, nossa cliente, pode esolher o que quiser!
-      </p>
-    </div>
-    <img
-      src="images/home4.png" 
-      alt="Imagem da execução do serviço de cílios: Destaque de um procedimento"
-      className="image"
-    />
-  </div>
+            <p>
+              No Studio Evelyn Gomes Cílios e Sobrancelhas trabalhamos sempre com segurança e cautela, respeitando os limites dos cílios de nossas clientes.
+            </p>
+            <p>
+              Todos os nossos materiais são esterelizados e higienizados, além de um ambiente seguro e confortável.
+            </p>
+          </div>
+          <img
+            src="images/home2.png"
+            alt="Imagem da execução do serviço de cílios: Destaque de um procedimento"
+            className="image"
+          />
+        </div>
+        {/* Seção da terceira imagem com texto */}
+        <div className="section">
+          <img
+            src="images/home3.png"
+            alt="Imagem da execução do serviço de cílios: Destaque de um procedimento"
+            className="image"
+          />
+          <div className="textSection">
+            <h2 className="tituloh2">Labios mais Harmonizados!</h2>
 
-</div>
+            <p>
+              Com o nosso serviço de micropigmentação labial, você terá os labios mais harmônicos e com mais cor!
+            </p>
+            <p>
+              Temos cores mais suaves, como um rosado mais natural até um vermelho marcante. Entre na nossa página de serviços disponível em nosso site e fique a vontade!
+            </p>
+          </div>
 
-{/* Seção da segunda imagem com texto */}
+        </div>
+          
+          {/* Seção da quarta imagem com texto */}
+        <div className="section">
+          <div className="textSection">
+            <h2 className="tituloh2">Sobrancelhas bem definidas!</h2>
 
+            <p>
+              Na categoria sobrancelhas, temos serviços de micropigmentação, design simples e design com henna.
+            </p>
+            <p>
+              Tudo depende da sua necessidade e o que mais te agrada!
+              No canto superior direito de nosso site, está disponível a página de serviços onde você, nossa cliente, pode esolher o que quiser!
+            </p>
+          </div>
+          <img
+            src="images/home4.png"
+            alt="Imagem da execução do serviço de cílios: Destaque de um procedimento"
+            className="image"
+          />
+        </div>
 
-  {/* Seção da segunda imagem com texto */}
-
-
-      <Footer/>
+      </div>
+      <Footer />
     </>
   );
 }
