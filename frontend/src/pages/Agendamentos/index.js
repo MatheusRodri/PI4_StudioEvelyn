fdc5b6b109c87c6bcf3b62cc042a904c48bb88a6
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2024-12-05 21:24:14 -0300

    correção

--- frontend/src/pages/Agendamentos/index.js
+++ frontend/src/pages/Agendamentos/index.js
@@ -21,22 +21,27 @@ function AgendamentoDetalhe() {
   });
 
   const fetchUserDetails = async () => {
-    const cachedUser = JSON.parse(localStorage.getItem('usuario'));
+
+    let cachedUser = JSON.parse(localStorage.getItem('usuario'));
+
+    if (cachedUser.email == 'adm@adm.com') {
+      cachedUser = null
+    }
     if (cachedUser) {
       setUsuario(cachedUser);
+
     } else if (!auth.currentUser) {
       nav('/login');
+
     } else {
       const user = auth.currentUser;
-
       if (!user.displayName) {
+
         auth.onAuthStateChanged(async (user) => {
           if (user) {
             const refDoc = doc(db, 'users', user.uid);
             const docSnap = await getDoc(refDoc);
-
             if (docSnap.exists()) {
-              console.log(docSnap.data());
               setUsuario(docSnap.data());
               localStorage.setItem('usuario', JSON.stringify(docSnap.data()));
             } else {
@@ -47,6 +52,7 @@ function AgendamentoDetalhe() {
           }
         });
       } else {
+        console.log('Usuário tem nome definido 4');
         setUsuario(user);
       }
     }
@@ -89,6 +95,7 @@ function AgendamentoDetalhe() {
   }, []);
 
   useEffect(() => {
+    console.log(usuario);
     if (usuario && usuario.permissao === 1) {
       carregarTodosAgendamentos();
     } else {
